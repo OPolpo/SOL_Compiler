68dbee86d117ec0cd1674a4d8426881ad931d6b1
Martinelli Ilaria <ilariamarty@gmail.com>
2014-05-07 15:36:20 +0200

    def.h finished

--- def.h
+++ def.h
@@ -1,1 +1,72 @@
-#define CHAR 256
+//
+//  def.h
+//
+//
+//  Created by Ilaria Martinelli on 07/05/14.
+//
+//
+
+#ifndef _def_h
+#define _def_h
+
+#define CHAR	256
+#define INT		257
+#define REAL	258
+#define STRING	259
+#define BOOL	260
+
+#define STRUCT	261
+#define VECTOR	262
+#define OF		263
+
+#define TYPE	264
+#define VAR		265
+#define CONST	266
+
+#define FUNC	267
+#define TOINT	268
+#define TOREAL	269
+
+#define REL_OP	270
+#define EQ		271
+#define NE		272
+#define LT		273
+#define LE		274
+#define GT		275
+#define GE		276
+#define IN 		277
+
+#define LOG_OP	278
+#define AND		279
+#define OR		280
+#define NOT		281
+
+#define IF		282
+#define THEN	283
+#define ELSE	284
+#define ELSIF	285
+#define ENDIF	286
+
+#define WHILE	287
+#define DO		288
+#define ENDWHILE 289
+
+#define FOR 	290
+#define TO 		291
+#define ENDFOR 	292
+
+#define FOREACH 293
+#define ENDFOREACH 294
+
+#define RETURN	295
+
+#define READ	296
+#define RD 		297
+
+#define WRITE 	298
+#define WR 		299
+
+//#define TRUE	
+//#define FALSE
+
+#endif
