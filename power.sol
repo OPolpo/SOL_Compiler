47f4c69c84b87306e2c8be8d8aad4c69ac3e0e6f
OPolpo <git-projects@omnip.it>
2014-07-04 14:09:27 +0200

    pow test

---
+++ power.sol
@@ -0,0 +1,28 @@
+func call():real
+	--power
+	func pow(base: real; exponent:int;): real
+		var res:real;
+			i:int;
+			rational:bool;
+	begin pow
+		res=base;
+		if (exponent == 0) then
+			return 1.0;
+		elsif(exponent < 0) then
+			base = -base;
+			rational = true;
+		endif;
+		for i = 0 to exponent do 
+			res = res * base;
+		endfor;
+		if (rational) then
+			return 1.0 / res;
+		else
+			return res;
+		endif;
+	end pow
+begin call
+	write pow(2.0,10);
+	return 1.0;
+end call
+	
