40d2f8e57b705156ea7834a33ceb8c3ea2c91711
OPolpo <git-projects@omnip.it>
2014-07-08 16:08:00 +0200

    miss file added

---
+++ s_shared/mem_utils.c
@@ -0,0 +1,21 @@
+#include "mem_utils.h"
+
+long size_allocated=0, size_deallocated=0;
+
+void mem_error(char * msg){
+    fprintf(stderr, "Mem error: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void * newmem(int size) {
+    void *p;
+    if((p = calloc(1,size)) == NULL) mem_error("Failure in memory allocation");
+    size_allocated += size;
+    return p;
+}
+
+void freemem(char *p, int size) {
+    free(p);
+    size_deallocated += size;
+}
+
---
+++ test_gen_code/in.sol
@@ -0,0 +1,11 @@
+--test :P
+func tester2():int
+--var v: vector [2] of int; k,sum: int;
+	
+begin tester2
+	write toint(3.6);
+	write toreal(96);
+	write vector(true,false,true);
+	write (vector(1,2,3) in vector(vector(1,2,4),vector(1,2,3),vector(4,2,3)));
+	return 1;
+end tester2
