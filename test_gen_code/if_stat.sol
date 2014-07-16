02e0c671436b64a038d69e03d00e52611992bd97
Martinelli Ilaria <ilariamarty@gmail.com>
2014-07-16 13:40:20 +0200

    if

--- test_gen_code/if_stat.sol
+++ test_gen_code/if_stat.sol
@@ -1,21 +1,14 @@
-func tester(i:int;):int
-var j: int;
+func tester():int
+var i,j,k: int;
 begin tester
-j=99;
 
-
-if i==0 then
-	j=3;
-	for i=0 to 10 do
-	write j;
-endfor;
-
---elsif i>5 then
---	j=i-2;
---elsif i>4 then
---	j=i-1;
---else
---	j=i*3;
+if i==j then
+	j=j+3;
+elsif i>j then
+   	i=i-2;
+   	j=i+k;
+else
+  	i=j*k;
 endif;
 
 return j;
