f26ac13e1a37af9402c3becf08ca5e0de51a7cd1
OPolpo <git-projects@omnip.it>
2014-07-16 15:31:51 +0200

    f4 checkwin works

--- test_gen_code/F4.sol
+++ test_gen_code/F4.sol
@@ -61,60 +61,108 @@ func F4():bool
 			return true;
 		end printheader
 
---	func checkwin():bool
---		var res:bool;
---
---		func checkrows():bool
---			var i,j, count:int;
---			begin checkrows
---				count = 0;
---				write "cicle\n";
---				for i=0 to 5 do
---					for j=1 to 6 do
---						--if( board[j][i] == board[j-1][i]) then
---					--	write i;
---					--	write "---";
---					--	write j;
---					--	write "\n";
---						dummy = j==1 ;
---						if true then
---							write board;
-							--
---						--else
---						--	write "false";
---						--	write board;
---						endif;
---					----	--if( board[j][i] == 1) then
-----
---					--	--dummy = board[j][i] == board[j-1][i];
-----
---					--	--	--count = count + 1;
---					--	--	--if count == 4 then
---					--	--	--	return true;
---					--	--	--endif;
---					--	--endif;
---					--	write i;
---					--	write "---";
---					--	write j;
---					--	write "\n";
---						dummy=false;
---					endfor;
---				endfor;
---				return false;
---			end checkrows
---		func checkcols():bool
---			begin checkcols
---				return false;
---			end checkcols
---		func checkdiag():bool
---			begin checkdiag
---				return false;
---			end checkdiag
---
---		begin checkwin
---			res = checkrows() or checkcols() or checkdiag();
---			return false;
---		end checkwin
+	func checkwin():bool
+		var res:bool;
+
+		func checkrows():bool
+			var i, j, count:int;
+			begin checkrows
+				for i=0 to 5 do
+				count = 0;
+					for j=1 to 6 do
+						if(board[j][i] != 0 and board[j][i] == board[j-1][i]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+				return false;
+			end checkrows
+		func checkcols():bool
+			var i, j, count:int;
+			begin checkcols
+				for j=0 to 6 do
+				count = 0;
+					for i=1 to 5 do
+						if(board[j][i] != 0 and board[j][i] == board[j][i-1]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+				return false;
+			end checkcols
+		func checkdiag():bool
+			var i, d, count:int;
+			begin checkdiag
+				for d=1 to 3 do
+				count = 0;
+					for i=1 to 5 - (d-1) do
+						if(board[i+d][i] != 0 and board[i+d][i] == board[i-1+d][i-1]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+				for d=1 to 3 do
+				count = 0;
+					for i=1 to 5 - (d-1) do
+						if(board[6-(i+d)][i] != 0 and board[6-(i+d)][i] == board[6-(i-1+d)][i-1]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+				for d=1 to 3 do
+				count = 0;
+					for i=1 to 5 - (d-1) do
+						if(board[i][i+(d-1)] != 0 and board[i][i+(d-1)] == board[i-1][i+(d-1)-1]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+				for d=1 to 3 do
+				count = 0;
+					for i=1 to 5 - (d-1) do
+						if(board[6-i][i+(d-1)] != 0 and board[6-i][i+(d-1)] == board[6-(i-1)][i+(d-1)-1]) then
+							count = count + 1;
+							if count == 3 then
+								return true;
+							endif;
+						else
+							count = 0;
+						endif;
+					endfor;
+				endfor;
+
+				return false;
+			end checkdiag
+
+		begin checkwin
+			res = checkrows() or checkcols() or checkdiag();
+			return res;
+		end checkwin
 
 	func printwinner(player:int;):bool
 		begin printwinner
@@ -146,11 +194,12 @@ func F4():bool
 			read userchoice;
 			if (userchoice>0 and userchoice<8) then
 				if(insertoken(userchoice, player)) then
-					--if checkwin() then
-					--	dummy = printwinner(player);
+					if checkwin() then
+						dummy = printwinner(player);
+						return true;
 						--return true;
-					--endif;			
-					dummy = cls();
+					endif;			
+					--dummy = cls();
 					dummy = printheader(player);
 					dummy = printboard();
 					dummy=false;
