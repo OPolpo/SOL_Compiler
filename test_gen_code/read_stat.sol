35f40ed4ad372a041ecf628f50b94d4ccfdba16b
OPolpo <git-projects@omnip.it>
2014-07-17 11:45:12 +0200

    read stat

--- test_gen_code/read_stat.sol
+++ test_gen_code/read_stat.sol
@@ -1,11 +1,12 @@
 func tester():int
+type game:struct(board: vector [7] of vector [6] of int; properclose: bool; player: int;);
 var --k:int;
-	v:struct(board: vector [7] of vector [6] of int; properclose: bool; player: int;);
+	v:game;
 begin tester
 	--v = struct(vector(vector(1, 2, 1, 2, 1, 2), vector(1, 2, 1, 2, 1, 2), vector(1, 2, 1, 2, 1, 2), vector(0, 0, 0, 0, 0, 0), vector(0, 0, 0, 0, 0, 0), vector(0, 0, 0, 0, 0, 0), vector(0, 0, 0, 0, 0, 0)),true,1);
 	--write ["a.txt"] v;
 	--write v;
 	read ["a.txt"] v;
-	write v;
+	--write v;
 	return 100;
 end tester
