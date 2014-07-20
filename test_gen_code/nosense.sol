065119e2d75b295a3bd42c0ceed95fd40096ba4f
OPolpo <git-projects@omnip.it>
2014-07-20 22:00:55 +0200

    other tests

---
+++ test_gen_code/nosense.sol
@@ -0,0 +1,23 @@
+-- no sense
+func f2(): int 
+var a,b,c:int;
+	d,e,f:real;
+	ff:bool;
+
+	begin f2
+
+		ff = false or rd bool;
+--		if(rd bool) then
+--			write toreal(toint(rd real));
+--		else
+--			if(rd bool) then
+--				if(rd bool) then
+--					if rd bool then
+--						write if(rd bool) then "vero" else "falso" endif;
+--					endif;
+--				endif;
+--			endif;	
+--		endif;
+		write "\n";
+		return 0;
+	end f2
